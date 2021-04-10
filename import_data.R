f7912129c08b9dd24a6377d4ca462c3441415967
Andreas Rutishauser <rutishauser.andreas@gmail.com>
2021-04-10 18:42:48 +0200

    feat: code to import games and shots via functions

---
+++ import_data.R
@@ -0,0 +1,162 @@
+
+# load functions
+source("functions/functions.R")
+
+
+
+################################################################################
+# import games
+################################################################################
+
+# # import all games
+# years <- 2014:2020
+# for (i in years){
+#   import_games(i)
+# }
+
+# import (update) current season
+import_games()
+games2020 <- readRDS("data/games_2020.rds")
+View(games2020$Bundesliga)
+
+
+################################################################################
+# import shots
+################################################################################
+
+# # import all shots
+# years <- 2016:2019
+# for (i in years){
+#   import_shots(i)
+# }
+
+import_shots(2016)
+shots2015 <- readRDS("data/shots_2015.rds")
+shots2020 <- readRDS("data/shots_2020.rds")
+shots2016 <- readRDS("data/shots_2016.rds")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################################
+# get tables - TEST
+################################################################################
+
+table_pl_2021 <- get_table(season = 2020, league = "Premier League")
+table_pl_0809 <- get_table(season = 2008, league = "Premier League")
+
+table_bl_2021 <- get_table(season = 2020, league = "Bundesliga")
+table_bl_0809 <- get_table(season = 2008, league = "Bundesliga")
+
+table_ll_0809 <- get_table(season = 2008, league = "La Liga")
+
+table_sa_1011 <- get_table(season = 2010, league = "Serie A")
+
+table_l1_1314 <- get_table(season = 2013, league = "Ligue 1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# EPL <- list()
+# La_liga <- list()
+# Bundesliga <- list()
+# Serie_A <- list()
+# Ligue_1 <- list()
+# 
+# for (i in seasons) {
+#   EPL[[i]] <- get_games(league = "EPL", season = i)
+#   La_liga[[i]] <- get_games(league = "La_liga", season = i)
+#   Bundesliga[[i]] <- get_games(league = "Bundesliga", season = i)
+#   Serie_A[[i]] <- get_games(league = "Serie_A", season = i)
+#   Ligue_1[[i]] <- get_games(league = "Ligue_1", season = i)
+#   
+#   save(EPL, file = "data/games_epl.RData")
+#   save(La_liga, file = "data/games_laliga.RData")
+#   save(Bundesliga, file = "data/games_bundesliga.RData")
+#   save(Serie_A, file = "data/games_seriea.RData")
+#   save(Ligue_1, file = "data/games_ligue1.RData")
+# }
+
+
+
+################################################################################
+# Get games tries
+################################################################################
+
+
+# for (i in seasons) {
+#   
+#   assign(x = i, sapply(leagues, function(x) NULL))
+#   
+#   for (j in leagues) {
+#     
+#     tmp_df <- get_games(league = j, season = i)
+#     tmp_name <- j
+#     assign(x = tmp_name, tmp_df)
+#     
+#   }
+# }
+
+# for (i in seasons) {
+#   
+#   for (j in leagues) {
+#     
+#     tmp_df <- get_games(league = j, season = i)
+#     tmp_name <- paste(j, i, sep = "_")
+#     
+#     print(paste0(tmp_name, "..."))
+#     assign(x = tmp_name, tmp_df)
+# 
+#   }
+# }
+
+# for (i in seasons) {
+#   
+#   tmp_name <- paste0("season_", i)
+#   assign(x = tmp_name, sapply(leagues, function(x) NULL))
+#   
+#   for (j in leagues) {
+#     tmp_df <- get_games(league = j, season = i)
+#     print(head(tmp_df))
+#     # tmp[[j]] <- tmp_df
+#     assign(tmp_name[[j]], tmp_df)
+#   }
+# }
+
+# for (i in seasons) {
+#   for (j in leagues) {
+#     tmp_name <- paste("season", i, j, sep = "_")
+#     assign(x = tmp_name, sapply(leagues, function(x) get_games(league = j, season = i)))
+#   }
+# }
